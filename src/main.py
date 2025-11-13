0a6c6240a3825621522d8e22dd6f76c6db04017c
blu-Encelado <bluencelado@gmail.com>
2025-11-13 18:09:31 +0100

    first checks

--- src/main.py
+++ src/main.py
@@ -1,1 +1,25 @@
-print("hello Basterd")
+import sys
+
+
+def main():
+    print("#####----Get a Password----#####")
+    comand = sys.argv
+
+    word = comand[1]
+    number = comand[2]
+        
+    for letter in word:
+        if not letter.isalpha():
+            raise ValueError("Please use a word with only letters as first argument")
+
+    for digit in number:
+        if not digit.isnumeric():
+            raise ValueError("Please use a number as second argument")
+
+    print(f"{word} // {number}")
+
+
+
+
+if __name__ == "__main__":
+    main()
