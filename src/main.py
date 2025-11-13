dc331bc5ce5b5f0f4a39203ef942ebbe244c7136
blu-Encelado <bluencelado@gmail.com>
2025-11-13 23:22:22 +0100

    optimization

--- src/main.py
+++ src/main.py
@@ -1,52 +1,32 @@
-import sys
 from config import *
 
 def main():
     print("#####----Get a Password----#####")
     
-    word_is_valid = False
-    num_is_valid = False
-    
-    while not word_is_valid:
+    while True:
         
         print("write a word (letters only)")
         read_word = input()
 
-        if read_word != "":
-            i = 0
-            for letter in read_word:
-                if not letter.isalpha():
-                    i += 1
+        if read_word.isalpha():
+            break
             
-            if i != 0:
-                print("Please use a word with only letters as first argument, try again")
-                    
-            word_is_valid = (i == 0)
-        print("word needed")
-                
+        print("Please use one word with only letters as first argument, try again")
+    
     print(f"the word is: {read_word}")
 
-    while not num_is_valid:
+    while True:
         
         print("write a number (digits only)")
         read_num = input()
 
-        if read_num != "":
-            j = 0
-            for digit in read_num:
-                if not digit.isnumeric():
-                    j += 1
+        if read_num.isnumeric():
+            break
             
-            if j != 0:
-                print("Please use a number as second argument, try again")
-                
-            num_is_valid = (j == 0)
-        print("number needed")
-        
+        print("Please use one number as second argument, try again")
         
     print(f"the arguments are: {read_word} and {read_num}")
-
-
+    print("==============================================")
 
 if __name__ == "__main__":
     main()
